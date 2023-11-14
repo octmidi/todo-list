399e44deaadbd3d734be0c958fc29a8ae37e205d
Daniel G. <octmidi@hotmail.com>
2023-11-14 19:28:38 -0300

    second validacion regex and clean input

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ function App() {
   const [listItems, setListItems] = useState([]);
 
   const handleInputChange = (e) => {
-    setInputText(e.target.value);
+    const capitalizedText = e.target.value
+      .toLowerCase()  // Convierte todo a minúsculas
+      .replace(/\b\w/g, (char) => char.toLocaleUpperCase());  // Convierte la primera letra de cada palabra en mayúscula
+
+    setInputText(capitalizedText);
+
 
 
   }
@@ -25,13 +30,16 @@ function App() {
   };
 
   const handleKeyPress = (e) => {
-    const regex = /^(?=.*[a-zA-Z]{4})[\w\d]*$/;
+    const regex = /^(?=[a-zA-Z]{2})[\s\w\d]*$/;
 
     if (e.key === 'Enter') {
       if (regex.test(inputText)) {
+
         handleAddItem();
       } else {
-        alert('La cadena no cumple con los requisitos mínimos.');
+        setInputText('');
+        alert('Las cadena no cumple con los requisitos mínimos. Los primeros dos caracteres deben ser letras');
+
       }
     }
 
@@ -43,34 +51,36 @@ function App() {
     setListItems(updatedList);
   }
 
-
-
   return (
-    <div className='list'>
+    <>
+      <div className='div-back'><h1>Todos</h1></div>
+      <div className='list'>
 
-      <ListGroup className='group'>
+        <ListGroup className='group'>
 
-        <InputGroup className="mb-3">
-          <FormControl
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyPress}
-            placeholder="Ingrese un elemento y presione Enter"
-          />
+          <InputGroup className="mb-3">
+            <FormControl
+              type="text"
+              value={inputText}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyPress}
+              placeholder="Ingrese un elemento y presione Enter"
+            />
 
-        </InputGroup>
-        {listItems.map((item, index) => (
-          <ListGroup.Item key={index} className='left-aligned-item'>{item}
-            <Badge bg="Light" pill className='icon' onClick={() => handleRemoveItem(index)}>
-              '❌'
-            </Badge>
+          </InputGroup>
+          {listItems.map((item, index) => (
+            <ListGroup.Item key={index} className='left-aligned-item'>{item}
+              <Badge bg="Light" pill className='icon' onClick={() => handleRemoveItem(index)}>
+                '❌'
+              </Badge>
 
-          </ListGroup.Item>
+            </ListGroup.Item>
 
-        ))}
-      </ListGroup>
-    </div>
+          ))}
+        </ListGroup>
+      </div>
+    </>
+
   )
 }
 
